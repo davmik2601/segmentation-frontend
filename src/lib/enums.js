d827bb237c95e8d186ee08475535509c78f8b5e7
davmik_skillex <david.mikayelyan@skillex.am>
2026-02-02 18:05:12 +0400

    add net_result event

--- src/lib/enums.js
+++ src/lib/enums.js
@@ -1,6 +1,6 @@
 export const ENUMS = {
   connectors: ['and', 'or'],
-  events: ['deposit', 'withdrawal', 'login'],
+  events: ['deposit', 'withdrawal', 'login', 'net_result'],
   aggregations: ['some', 'every', 'sum', 'avg', 'min', 'max', 'count'],
   metrics: ['amount'],
   operators: ['eq', 'neq', 'gt', 'gte', 'lt', 'lte', 'between', 'not_between'],
